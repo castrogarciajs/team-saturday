609e496ca90c48e883e29d649cfad5edc1f20d9e
Kelvin-orcasita <klvorcasita@gmail.com>
2023-11-04 18:01:08 -0500

    style button

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -47,7 +47,12 @@ export function LoginPage() {
                 required
               />
             </label>
-            <button type="submit">Login</button>
+            <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    Login
+                  </button>
           </form>
           <a href="#">Google</a>
         </div>
