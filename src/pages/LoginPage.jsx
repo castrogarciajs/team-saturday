6a3417f9125ad89190b37971b5527eaa4a8ec641
westhly <westhly-carrillo@outlook.com>
2023-11-04 17:37:54 -0500

    intall prettier

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,39 +1,40 @@
-import {registerUser} from '../firebase/app.js'
-import {useRef, useState} from 'react'
-import {useNavigate} from'react-router-dom'
-
+import { registerUser } from "../firebase/app.js";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function LoginPage() {
-    const [messageError, setMessageError] = useState('')
-    const form = useRef(null)
-    const navigate = useNavigate()
-  
-    const handleSubmit = (event) => {
-      event.preventDefault()
-      if(!form.current) return
-      if(!form.current.email.value) return setMessageError('Email is required')
-      if(!form.current.password.value) return setMessageError('Password is required')
-      if(form.current.password.value.length < 6) return setMessageError('Password must be at least 6 characters')
-      setMessageError('')
-      registerUser(form.current.email.value, form.current.password.value)
-      form.current.reset()
-      navigate('/')
-    }
-     
-    return (
-        <>
-          <h1>Authenticacion</h1>
-          <small>{messageError}</small>
-          <form action="#" ref={form} onSubmit={handleSubmit}>
-            <label htmlFor="email">
-              <input type="email" name="email" required />
-            </label>
-            <label htmlFor="password">
-              <input type="password" name="password" required />
-            </label>
-            <button type="submit">Login</button>
-          </form>
-          <a href="#">Google</a>
-        </>
-      )
+  const [messageError, setMessageError] = useState("");
+  const form = useRef(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!form.current) return;
+    if (!form.current.email.value) return setMessageError("Email is required");
+    if (!form.current.password.value)
+      return setMessageError("Password is required");
+    if (form.current.password.value.length < 6)
+      return setMessageError("Password must be at least 6 characters");
+    setMessageError("");
+    registerUser(form.current.email.value, form.current.password.value);
+    form.current.reset();
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>Authenticacion</h1>
+      <small>{messageError}</small>
+      <form action="#" ref={form} onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          <input type="email" name="email" required />
+        </label>
+        <label htmlFor="password">
+          <input type="password" name="password" required />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+      <a href="#">Google</a>
+    </>
+  );
 }
