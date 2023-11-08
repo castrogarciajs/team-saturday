ccc2b9fe90a57440bdcce8e95885273b6ce982b0
Kelvin-orcasita <klvorcasita@gmail.com>
2023-11-07 22:03:06 -0500

    update

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -6,32 +6,32 @@ export function RegisterPage() {
   const form = useRef(null)
   const [messageError, serMessageError] = useState('')
 
-function handleSubmit(event) {
-  event.preventDefault()
-  if(!form.current) return
-  if(form.current.password.value === form.current.repeatPassword.value){
-    if(!form.current.username.value) return serMessageError('Username is required') 
-    if(!form.current.password.value) return serMessageError('Password is required') 
-    if(form.current.password.value.length < 6) return serMessageError('Password must be at last 6 characters') 
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (!form.current) return
+    if (form.current.password.value === form.current.repeatPassword.value) {
+      if (!form.current.username.value) return serMessageError('Username is required')
+      if (!form.current.password.value) return serMessageError('Password is required')
+      if (form.current.password.value.length < 6) return serMessageError('Password must be at last 6 characters')
 
-    registerUser(form.current.username.value, form.current.password.value)
-    form.current.reset()
-    serMessageError('')
-    return
+      registerUser(form.current.username.value, form.current.password.value)
+      form.current.reset()
+      serMessageError('')
+      return
+    }
+    return serMessageError('password do not match')
   }
-  return serMessageError('password do not match')
-}
 
   return (
     <>
       <section>
         <article>
-          <div className="flex justify-center h-screen items-center bg-slate-200">
-            <div className="w-full max-w-xs">
-              
-              <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4 " ref={form} onSubmit={handleSubmit}>
-              <b className="block text-gray-700 text-2xl text-center font-bold mb-6">Create an account</b>
-                <div className="mb-4">
+          <div className='flex justify-center h-screen items-center bg-slate-200'>
+            <div className='w-full max-w-xs'>
+
+              <form className='bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4' ref={form} onSubmit={handleSubmit}>
+                <b className='block text-gray-700 text-2xl text-center font-bold mb-6'>Create an account</b>
+                <div className='mb-4'>
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
                     htmlFor="username"
@@ -48,7 +48,7 @@ function handleSubmit(event) {
                 <div>
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="password" 
+                    htmlFor="password"
                   >
                     Password
                   </label>
@@ -62,7 +62,7 @@ function handleSubmit(event) {
                 <div className="mb-6">
                   <label
                     className="block text-gray-700 text-sm font-bold mb-2"
-                    htmlFor="password" 
+                    htmlFor="password"
                   >
                     Repeat password
                   </label>
@@ -72,7 +72,7 @@ function handleSubmit(event) {
                     type="text"
                     placeholder="******************" autoComplete="off"
                   />
-                  
+
                   <hr />
                   <p className="text-red-500 text-xs italic mb-2">
                     {messageError}
@@ -82,7 +82,7 @@ function handleSubmit(event) {
                   <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="submit"
-                  > 
+                  >
                     Create
                   </button>
                   <a
